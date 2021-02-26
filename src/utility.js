c3043e5ff21ae3c7dc911f248fe1e1f6f0ac8d56
Sean McClure <seanaaron100@gmail.com>
2021-02-26 05:23:46 +0000

    adding synonym vectors2

--- src/utility.js
+++ src/utility.js
@@ -4,12 +4,19 @@ import {figue} from "./libs/figue.js";
 
 var thesaurus = require('./data/thes.json');
 
+window.full_vocab = [];
+
+Array.prototype.diff = function(a) {
+    return this.filter(function(i) {return a.indexOf(i) < 0;});
+};
+
 export const utility = {
     pipeline: function() {
         var res = {}
         var tweets = utility.read_tweets()
         var synms = utility.add_synonyms_to_tweets(tweets)
-        console.log(synms)
+        var fin = utility.distance_between_all_vectors(synms)
+        console.log(fin)
     },
     read_tweets: function() {
         var cleaned_tweets = []
@@ -29,7 +36,7 @@ export const utility = {
         return(res.join(" "))
     },
     remove_special_chars: function(tweet) {
-        var res = tweet.split("“").join("").split("/").join(" ").split("_").join("").split('"').join("").split("*").join("").split("(").join("").split(")").join("").split("#").join("").split("?").join("").split(";").join("").split(":").join("").split(",").join("").split("’").join("'").split("1").join("").split("2").join("").split("3").join("").split("4").join("").split("5").join("").split("6").join("").split("7").join("").split("8").join("").split("9").join("").split("0").join("")
+        var res = tweet.split("“").join("").split("/").join(" ").split("_").join("").split('"').join("").split("*").join("").split("(").join("").split(")").join("").split("#").join("").split("?").join("").split(";").join("").split(":").join("").split(",").join("").split("’").join("'").split("1").join("").split("2").join("").split("3").join("").split("4").join("").split("5").join("").split("6").join("").split("7").join("").split("8").join("").split("9").join("").split("0").join("").split(".").join("")
         return (res)
     },
     find_longest_word: function(str, num_words) {
@@ -71,14 +78,16 @@ export const utility = {
         const page = document.body.innerHTML;
         document.body.innerHTML = page.replace(new RegExp(word, "gi"), (match) => `<mark>${match}</mark>`);
     },
-    build_vocabulary : function(topics) {
-        var vocab = [];
-        topics.forEach(function(topic_arr) {
-            topic_arr.forEach(function(word) {
-                vocab.push(word);
-            })
+    build_vocab_vector : function(synonym_vector) {
+        var res = []
+        window.full_vocab.forEach(function(word) {
+             if(synonym_vector.includes(word)) {
+                res.push(1)
+             } else {
+                res.push(0)
+             }
         })
-        return(vocab)
+        return(res)
     },
     find_synonyms : function(word) {
         var res = {};
@@ -129,10 +138,33 @@ export const utility = {
             inner["synonyms"].forEach(function(obj, i) {
                 all_syms.push(obj.synonyms)
             })
-            inner["synonym_vector"].push(all_syms.join(",") + keep_words.join(","))
+            var all_words = all_syms.join(",") + keep_words.join(",");
+            inner["synonym_vector"].push(all_words)
+            all_words.split(",").forEach(function(word) {
+                window.full_vocab.push(word)
+            })
         res.push(inner)
     })
+        res.forEach(function(obj) {
+            obj["vocab_vector"] = utility.build_vocab_vector(obj.synonym_vector)
+        })
         return(res)
+    },
+    euclidean_between_vectors : function(a1, a2, startIndex, len) {
+        var result = 0, i, diff;
+        for (i = startIndex; i < (startIndex + len); i += 1) {
+            diff = a1[i] - a2[i];
+            result += diff * diff;
+        }
+        return Math.sqrt(result);
+    },
+    distance_between_all_vectors : function(synms) {
+        var all_vecs = []
+        synms.forEach(function(obj) {
+            all_vecs.push(obj.vocab_vector)
+        })
+        var all_distances = all_vecs.flatMap((v, i) => all_vecs.slice(i+1).map( w => v.diff(w).length));   // utility.euclidean_between_vectors(v, w, 0, 2)
+        return(all_distances)
     },
     fetch_vector : function(dtm, index) {
         var props_only = Object.fromEntries(Object.entries(dtm[index]).filter(([key, value]) => key !== "original_tweet" && key !== "cleaned_tweet") )
@@ -143,8 +175,8 @@ export const utility = {
         var labels = new Array;
         var vectors = new Array;
         for (var i = 0; i < data.length; i++) {
-            labels[i] = data[i]['original_tweet'];
-            vectors[i] = utility.fetch_vector(data, i)
+            labels[i] = data[i]["original_tweet"]
+            vectors[i] = data[i]["vocab_vector"]
         }
         var clusters = figue.kmeans(k, vectors);
         var kmeans_results = []
